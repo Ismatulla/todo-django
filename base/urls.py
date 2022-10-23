f1bd16dbdc66f36162820c081ed293394262558a
Ismatulla <ismatilloqoyliyev@gmail.com>
2022-10-23 18:50:24 +0200

    todo-app

---
+++ base/urls.py
@@ -0,0 +1,16 @@
+from django.urls import path
+
+from .views import TaskList,TaskDetail,CreateTask,TaskUpdate,TaskDelete,TaskLogin,RegisterPage
+
+from django.contrib.auth.views import LogoutView
+
+urlpatterns=[
+  path('register/',RegisterPage.as_view(),name='register'),
+  path('login/',TaskLogin.as_view(),name='login'),
+  path('logout/',LogoutView.as_view(next_page='login'),name='logout'),
+  path('',TaskList.as_view(),name='tasks'),
+  path('task/<int:pk>/',TaskDetail.as_view(),name='task'),
+  path('create-task/',CreateTask.as_view(),name='create-task'),
+  path('update-view/<int:pk>/',TaskUpdate.as_view(),name='update-view'),
+  path('delete-view/<int:pk>/',TaskDelete.as_view(), name='delete-view')
+]
